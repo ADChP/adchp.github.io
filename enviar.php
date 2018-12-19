ce514abdfcbb8e0a8eba86ba215206dc667cb972
Andrés <46004074+ADChP@users.noreply.github.com>
2018-12-19 13:05:33 -0600

    Delete enviar.php

--- enviar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$destino= 'chavacr93@gmail.com';
-$nombre= $_POST['name'];
-$correo= $_POST['email'];
-$mensaje= $_POST['message'];
-$contenido= 'Nombre: ' . $nombre . '\nCorreo' . $correo . '\nMensaje:' . $mensaje;
-mail($destino, 'Contacto', $contenido);
-header('Location:index.html');
-?>
